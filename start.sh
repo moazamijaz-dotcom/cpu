0411a1cf6d2008f4e5125942d96b6df1af88e901
moazamijaz-dotcom <73375949+moazamijaz-dotcom@users.noreply.github.com>
2021-03-14 16:37:48 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q3n0l8fp5v4a0vuy76xfrzer7sqc0p9nx85tcue.shuger
 
 
 done
